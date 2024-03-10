f45bc36ab0618890e98a88d16194c53990dc773d
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-03-10 08:18:36 +0800

    Update SUM.js

--- SUM.js
+++ SUM.js
@@ -19,9 +19,9 @@ export const squareONE = 'square % ONE'
 export const oppoPyramid = 'antipyramid / pyramid'
 export const dimPyramid = 'suqare - antisquare'
 void function SUM(){
-    void ['DET/tri+ tri/DET'==='DETtri + triDET']
-    void 'DETtri+ONEsquare==squareONE+triDET'
-    void 'Pyramid == oppoPyramid+dimPyramid'
+    void ['DET/tri+ tri/DET'===DETtri + triDET]
+    void [DETtri+ONEsquare==squareONE+triDET]
+    void [Pyramid == oppoPyramid+dimPyramid]
     return SUM
 }
 console.log(DETtri,triDET)
