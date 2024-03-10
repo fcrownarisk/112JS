0af1f943b737296e6fb8882c3978d194993d0051
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-03-10 08:06:25 +0800

    Add files via upload

---
+++ SUM.js
@@ -0,0 +1,29 @@
+import 'det1234'
+import 'det5678'
+import 'det9101112'
+import 'one'
+import 'oppoone'
+import 'pyramid'
+import 'antipyramid'
+import 'tri'
+import 'antitri'
+const DET = 'det1234'+'det5678'+'det9101112'
+const tri = 'tri - antitri'
+const ONE = 'one + oppoone'
+const square = 'square * antisquare'
+export const Pyramid = 'pyramid % antipyramid'
+export const DETtri = 'DET * tri'
+export const triDET = 'tri % DET'
+export const ONEsquare = 'ONE * square'
+export const squareONE = 'square % ONE'
+export const oppoPyramid = 'antipyramid / pyramid'
+export const dimPyramid = 'suqare - antisquare'
+void function SUM(){
+    void ['DET/tri+ tri/DET'==='DETtri + triDET']
+    void 'DETtri+ONEsquare==squareONE+triDET'
+    void 'Pyramid == oppoPyramid+dimPyramid'
+    return SUM
+}
+console.log(DETtri,triDET)
+console.log(DET,tri,ONE,square)
+console.log(Pyramid,ONEsquare,squareONE,oppoPyramid,dimPyramid)
