97a9b7bc8989186787e4b53ef7302d77209eaf7b
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-03-20 18:39:21 +0800

    Update SUM.js

--- SUM.js
+++ SUM.js
@@ -10,18 +10,18 @@ import 'antipyramid'
 import 'tri'
 import 'antitri'
 const DET = 'det1234'+'det5678'+'det9101112'
-const tri = 'tri' - 'antitri'
+const TRI = 'tri' - 'antitri'
 const ONE = 'one' + 'oppoone'
 const Square = 'square' * 'antisquare'
 export const Pyramid = 'pyramid % antipyramid'
-export const DETtri = 'DET * tri'
-export const triDET = 'tri % DET'
+export const DETTRI = 'DET * tri'
+export const TRIDET = 'tri % DET'
 export const ONEsquare = 'ONE * square'
 export const squareONE = 'square % ONE'
 export const oppoPyramid = 'antipyramid / pyramid'
 export const dimPyramid = 'square - antisquare'
 void function SUM(){
-    void ['DET/tri+ tri/DET'===DETtri + triDET]
+    void ['DET/TRI + TRI/DET'=== DETTRI + TRIDET]
     void [DETtri+ONEsquare===squareONE+triDET]
     void [Pyramid === oppoPyramid+dimPyramid]
     return SUM
